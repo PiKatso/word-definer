417dc2dc541dabcec270d559da05a8af2481c170
Kat Gifford <katgiff2007@gmail.com>
2017-04-28 11:12:15 -0700

    start on front end routing and pages

--- app.rb
+++ app.rb
@@ -2,9 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './lib/word_builder'
 require 'pry'
-
 also_reload('lib/**/*.rb')
 
 get('/') do
+  # @output = Words.all
   erb(:index)
+end
+
+get('/word/add-word') do
+  erb(:form)
+end
+
+post('/word/new-word') do
+  word_input = params.fetch("word_input")
+  define_term = params.fetch("define_word")
+
+  new_word_hash = {:name => word_input, :definition => define_term}
+  new_word = Word.new(new_word_hash)
+  new_word.save
+  erb(:success)
 end
