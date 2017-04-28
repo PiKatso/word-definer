1412381545bfd3952da1a0eb171b41fedd1f86e6
Kat Gifford <katgiff2007@gmail.com>
2017-04-28 11:58:03 -0700

    add second integration spec

--- spec/word_builder_integration_spec.rb
+++ spec/word_builder_integration_spec.rb
@@ -9,4 +9,12 @@ describe("index path", {:type => :feature}) do
     visit("/")
     expect(page).to have_content("")
   end
+
+  it("/word/new-word") do
+    visit("/word/add-word")
+    fill_in('word_input', :with => 'hi')
+    fill_in('define_word', :with => 'hello')
+    click_button('Submit')
+    expect(page).to have_content("")
+  end
 end
